47c909481ecb37c060c363f13ae775f352c5536c
Peter <Peter92@users.noreply.github.com>
2015-03-12 01:40:52 +0000

    Update Octree.py
    Tried using a 3x3x3 grid but it doesn't work (there's a reason why it's called an octree), although it should be a lot easier to switch sizes now

--- Octree.py
+++ Octree.py
@@ -26,119 +26,68 @@ def editDictionary( dictionaryName, listOfValues, canOverwriteKeys=True ):
     else:
         return False
         
-class roundToMultiple:
-    def __init__( self, multiple, *args ):
-        self.returnList = []
-        self.multiple = int( multiple )
-        self.args = args
-        self.mathFunction = math.ceil
-    def up( self ):
-        return self.round()
-    def down( self ):
-        self.mathFunction = math.floor
-        return self.round()
-    def round( self ):
-        for n in self.args:
-            self.returnList.append( int( self.mathFunction( n/float( self.multiple ) )*self.multiple ) )
-        return self.returnList
-
+def roundToMultiple( multiple, *args ):
+    maxPower = 0
+    for i in args:
+        if i:
+            try:
+                closestPower = int( math.ceil( math.log( abs( i ), multiple ) ) )
+            except:
+                closestPower = 0
+        else:
+            closestPower = 0
+        if closestPower > maxPower:
+            maxPower = closestPower
+    return maxPower
 
 grid = {}
-grid[(0,0)] = 1
-grid[(0,0)] = 1
-grid[(3,-1)] = 1
-grid[(9, -3)] = 1
+grid[(0,1,0)] = 1
+grid[(0,0,0)] = 1
+grid[(3,5,-1)] = 1
+grid[(10,-5,-3)] = 1
 
-#Convert to octree
+#Get maximum depth level
 xMax = max( grid.keys(), key=itemgetter( 0 ) )[0]
 xMin = min( grid.keys(), key=itemgetter( 0 ) )[0]
 yMax = max( grid.keys(), key=itemgetter( 1 ) )[1]
 yMin = min( grid.keys(), key=itemgetter( 1 ) )[1]
-    
-#Get maximum depth level
-minRange = roundToMultiple( 8, xMin, yMin ).down()
-maxRange = roundToMultiple( 8, xMax, yMax ).up()
-mainDepthLevel = max( [abs( i ) for i in minRange+maxRange] )
-mainDepthLevel = int( math.log( mainDepthLevel/2, 2 ) )
-minDepthLevel = 0 #0 has accuracy of 1, -1 of 0.5 etc
+zMax = max( grid.keys(), key=itemgetter( 2 ) )[2]
+zMin = min( grid.keys(), key=itemgetter( 2 ) )[2]
+minDepthLevel = 0
+maxDepthLevel = roundToMultiple( 3, xMax, xMin, yMax, yMin, zMax, zMin )
 
 #Start octree dictionary
-octreeStructure = [( 1, 1 ), ( 1, -1 ), ( -1, 1 ), ( -1, -1 )]
+octreeRange = ( 1, 0, -1 )
+octreeStructure = set()
+for x in octreeRange:
+    for y in octreeRange:
+        for z in octreeRange:
+            octreeStructure.add( ( x, y, z ) )
 octreeDepthName = "Depth"
 octreeDataName = "Data"
 octreeData = {"Depth":mainDepthLevel, "Data": dict.fromkeys( octreeStructure, False )}
 
 
-#originalCoordinates = dict.fromkeys( [tuple( x+0.5 for x in i ) for i in grid.keys()] )
 originalCoordinates = dict.fromkeys( grid.keys() )
-for relativeCoordinate in originalCoordinates.keys():
-    multiplierList = {0: [], 1: []}
-    exactMatch = {0: [], 1: []}
+for absoluteCoordinate in originalCoordinates.keys():
+    print absoluteCoordinate
+    multiplierList = dict.fromkeys( range( 3 ), list() )
     for key in multiplierList:
-        maxMultiplier = ( mainDepthLevel-1 )*4
+        maxMultiplier = 3**maxDepthLevel
         totalMultiplier = 0
-        while maxMultiplier > pow( 2, minDepthLevel )*0.9:
+        while maxMultiplier > pow( 3, minDepthLevel )*0.9:
             #Detect if it should be positive or negative
             currentMultiplier = maxMultiplier
-            if relativeCoordinate[key] >= totalMultiplier:
+            if relativeCoordinate[key] > totalMultiplier:
                 multiplierList[key].append( 1 )
-            else:
+            elif relativeCoordinate[key] < totalMultiplier:
                 multiplierList[key].append( -1 )
                 currentMultiplier *= -1
-            if relativeCoordinate[key] == totalMultiplier:
-                exactMatch[key].append( True )
             else:
-                exactMatch[key].append( False )
+                multiplierList[key].append( 0 )
+            print currentMultiplier
             #Append to total
             totalMultiplier += currentMultiplier
-            maxMultiplier /= 2.0
-    
-    originalCoordinates[relativeCoordinate] = multiplierList
-
-
-#Write into dictionary
-for relativeCoordinate in originalCoordinates:
-    
-    #Get the coordinates for each depth level
-    relativeValues = originalCoordinates[relativeCoordinate]
-    relativeCoordinates = zip( relativeValues[0], relativeValues[1] )
-    
-    #Fill with True
-    dictionaryFix = [("Data")]*( len( relativeCoordinates )*2 )
-    dictionaryFix[1::2] = relativeCoordinates
-    dictionaryFix.append( 1 )
-    editDictionary( octreeData, dictionaryFix )
-
-    #Fill empty values with False
-    currentDepth = 0
-    maxDepth = octreeData["Depth"]-minDepthLevel
-    while currentDepth < maxDepth:
-        depthDictionaryPath = dictionaryFix[:-1]
-        currentDictionaryDepth = reduce( dict.__getitem__, depthDictionaryPath[:-1-currentDepth*2], octreeData )
-        for i in octreeStructure:
-            if currentDictionaryDepth.get( i, None ) == None:
-                currentDictionaryDepth[i] = False
-                editDictionary( octreeData, depthDictionaryPath[:-1-currentDepth*2]+[i, False], False )
-        #Fill in depth
-        editDictionary( octreeData, depthDictionaryPath[:-2-currentDepth*2]+["Depth", currentDepth+minDepthLevel], False )
-        currentDepth += 1
-
-#Calculate placement of cubes
-def drawCubes( dictionaryValue, startingCoordinates=[0, 0] ):
-    currentDepth = dictionaryValue["Depth"]
-    depthMultiplier = pow( 2, currentDepth )
-    for key in dictionaryValue["Data"].keys():
-        newCoordinate = [depthMultiplier*i for i in key]
-        newCoordinate = [x+y for x, y in zip( newCoordinate, startingCoordinates )]
-        newDictionaryValue = dictionaryValue["Data"][key]
-        if newDictionaryValue == 1:
-            cubeSize = 2*2**currentDepth
-            newCube = py.polyCube( h=5, w=cubeSize, d=cubeSize )[0]
-            py.move( newCube, ( newCoordinate[0], 0, newCoordinate[1] ) )
-            
-        elif type( newDictionaryValue ) == dict:
-            drawCubes( newDictionaryValue, newCoordinate )
-
-#Use test value for a bigger cube
-editDictionary( octreeData, ["Data",(-1,-1),1] )
-drawCubes( octreeData )
+            maxMultiplier /= 3.0
+        print multiplierList
+        print
